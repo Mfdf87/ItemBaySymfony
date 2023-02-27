fcdc435976efe9d8e6ab105c3c61db8a9c4b2919
Matthieu FDF <mat.desforts@gmail.com>
2023-02-27 12:19:38 +0100

    Afficher user infos

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
         return $this->render('home.html.twig', [
-            'message' => 'bonjour'
+            'allah' => $entityManager->getRepository('App\Entity\User')->find(1),
         ]);
     }
 }
