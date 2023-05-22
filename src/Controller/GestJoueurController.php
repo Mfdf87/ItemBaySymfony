922daac3ade257bb304936533b2da3b927b8b231
TanguyVey <115634124+TanguyVey@users.noreply.github.com>
2023-05-22 10:48:04 +0200

    Bon page gestion compte est faites

--- src/Controller/GestJoueurController.php
+++ src/Controller/GestJoueurController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GestJoueurController extends AbstractController
 {
-    #[Route('/gest/joueur', name: 'app_gest_joueur')]
-    public function index(): Response
+    #[Route('/gest/joueur/{id}/{nom}/{prenom}/{password}/{email}', name: 'app_gest_joueur')]
+    public function index($id, $nom, $prenom, $password, $email): Response
     {
+        $compte = [
+            'id' => $id,
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'password' => $password,
+            'email' => $email,
+            // ...
+        ];
+
         return $this->render('pages/gest_joueur/index.html.twig', [
             'controller_name' => 'GestJoueurController',
+            'compte' => $compte,
         ]);
     }
 }
