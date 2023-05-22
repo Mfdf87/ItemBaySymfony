e06d234216c1709d5c66951179fd1c42140d8fdf
Matthieu FDF <mat.desforts@gmail.com>
2023-05-22 09:00:52 +0200

    Ajout form inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +15,16 @@ class InscriptionController extends AbstractController
         return $this->render('pages/inscription/index.html.twig', [
             'controller_name' => 'InscriptionController',
         ]);
+    }
+
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request): Response
+    {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        dd($email, $password);
+
+        return new Response('ok');
     }
 }
