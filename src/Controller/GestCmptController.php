38ca8e8fb82eab0ace85003dd51f95c86421a4ba
TanguyVey <115634124+TanguyVey@users.noreply.github.com>
2023-05-22 09:43:30 +0200

    Gestion cmpt

--- src/Controller/GestCmptController.php
+++ src/Controller/GestCmptController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\CompteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GestCmptController extends AbstractController
 {
-    #[Route('/gest/cmpt', name: 'app_gest_cmpt')]
-    public function index(): Response
+    #[Route('/gest/cmpt', name: 'app_gest_cmpt', methods: ['GET'])]
+    public function index(CompteRepository $repository): Response
     {
         return $this->render('pages/gest_cmpt/index.html.twig', [
             'controller_name' => 'GestCmptController',
+            'comptes' => $repository->findAll()
         ]);
     }
 }
