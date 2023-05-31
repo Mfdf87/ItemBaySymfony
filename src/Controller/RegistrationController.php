53c41241e795b899afc4162221bf21383be677f7
Matthieu FDF <mat.desforts@gmail.com>
2023-05-31 15:46:22 +0200

    Ajout infos formulaire inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,6 +30,11 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            $user->setNom($form->get('nom')->getData());
+            $user->setPrenom($form->get('prenom')->getData());
+            $user->setMonnaie(10);
+            $user->setRoles(['ROLE_USER']);
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -35,6 +35,8 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('nom')
+            ->add('prenom')
         ;
     }
 
