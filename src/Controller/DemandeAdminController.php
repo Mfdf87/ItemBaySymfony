a8391e3f67c8866b61c6a58a0a704d15414425aa
TanguyVey <115634124+TanguyVey@users.noreply.github.com>
2023-06-01 15:00:55 +0200

    Push front

--- src/Controller/DemandeAdminController.php
+++ src/Controller/DemandeAdminController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DemandeAdminController extends AbstractController
 {
     #[Route('/demande/admin', name: 'app_demande_admin')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('demande_admin/index.html.twig', [
+        return $this->render('pages/demande_admin/index.html.twig', [
             'controller_name' => 'DemandeAdminController',
+            'id' => $request->query->get('id'),
         ]);
     }
 }
